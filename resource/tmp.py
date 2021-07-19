3a7294a7cf374a615ee5e7919485e0311f7d41af
comar <saywooong@gmail.com>
2021-07-19 10:50:58 +0900

    update: auto

--- resource/tmp.py
+++ resource/tmp.py
@@ -1,7 +1,21 @@
 import regex
+
 texts = ['/home/comar/tmp.cmp',
+        './dkdk/foo.jpg',
         'foo.jpg',
+        'foo.kk.jpg',
         ]
 
+
+for text in texts:
+
+    directory = 'unknown'
+    file = 'unknown'
+    directory = regex.findall(r'[^/]+(?<=\.)',text)[0]
+    file = regex.findall(r'[^/]+', text)[-1]
+    print('------------------')
+    print('original : %s'%text)
+    print('file     : %s'%file)
+    print('dir      : %s'%directory)
 
 
