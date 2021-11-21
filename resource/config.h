34bebfd8e643b5b71184379d1050c86750f10f8f
Comar <32098205+KIMGEONUNG@users.noreply.github.com>
2021-11-21 12:42:59 +0900

    Add files via upload

---
+++ resource/config.h
@@ -0,0 +1,50 @@
+/*
+Copyright 2019 @foostan
+Copyright 2020 Drashna Jaelre <@drashna>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+/* Select hand configuration */
+
+#define MASTER_LEFT
+// #define MASTER_RIGHT
+// #define EE_HANDS
+
+#define QMK_KEYS_PER_SCAN 12
+#define USB_POLLING_INTERVAL_MS 1
+#define DEBOUNCE 0
+
+#define MOUSEKEY_INTERVAL 20
+#define MOUSEKEY_DELAY 0
+#define MOUSEKEY_TIME_TO_MAX 10
+#define MOUSEKEY_MAX_SPEED 3
+#define MOUSEKEY_WHEEL_DELAY 0
+
+#ifdef RGBLIGHT_ENABLE
+#    undef RGBLED_NUM
+#    define RGBLIGHT_ANIMATIONS
+#    define RGBLED_NUM 54
+#    undef RGBLED_SPLIT
+#    define RGBLED_SPLIT \
+        { 27, 27 }
+#    define RGBLIGHT_LIMIT_VAL 120
+#    define RGBLIGHT_HUE_STEP  10
+#    define RGBLIGHT_SAT_STEP  17
+#    define RGBLIGHT_VAL_STEP  17
+#endif
+
+#define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
---
+++ resource/rules.mk
@@ -0,0 +1,9 @@
+MOUSEKEY_ENABLE     = yes     # Mouse keys
+RGBLIGHT_ENABLE     = yes    # Enable WS2812 RGB underlight.
+VIA_ENABLE          = yes         # Enable VIA
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306
+LTO_ENABLE          = yes
+
+NKRO_ENABLE = yes
+DEBOUNCE_TYPE = sym_eager_pk
