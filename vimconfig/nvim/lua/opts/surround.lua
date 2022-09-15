9715b4cb6cc3e579ff849ed2e0ead32f2a9fcf69
comar <saywooong@gmail.com>
2022-09-16 08:25:39 +0900

    update: trivial

--- vimconfig/nvim/lua/opts/surround.lua
+++ vimconfig/nvim/lua/opts/surround.lua
@@ -1,10 +1,18 @@
 
+-- PYTHON-SPECIFIC
 vim.cmd([[
-
 autocmd FileType python let b:surround_{char2nr('p')} = "print(\r)"
-
-autocmd FileType tex let b:surround_{char2nr('b')} = "{\\bf \r}"
-autocmd FileType tex let b:surround_{char2nr('i')} = "{\\it \r}"
-
 ]])
 
+-- LATEX-SPECIFIC
+vim.cmd([[
+autocmd FileType tex let b:surround_{char2nr('b')} = "{\\bf \r}"
+autocmd FileType tex let b:surround_{char2nr('i')} = "{\\it \r}"
+]])
+
+-- BASH
+vim.cmd([[
+
+autocmd FileType sh let b:surround_{char2nr('v')} = "${\r}"
+
+]])
