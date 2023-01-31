a5f188b77b99ccfd068a2d1e177855af2a12db17
comar <saywooong@gmail.com>
2023-01-31 10:57:46 +0900

    feat: trivial

---
+++ docs/build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+scripts=`find . -type f | grep -v build.sh`
+
+targetPath="${HOME}/bin"
+system=`uname`
+
+if [[ -z $(echo $PATH | grep -o ${targetPath}:) ]]; then
+    PATH=$PATH:${targetPath}:
+fi
+
+if [ $system == "Linux" ]  
+then
+    chmod +x ./*
+fi
+
+if [[ ! -d "$targetPath" ]]
+then
+    mkdir ~/bin -v 
+fi
+
+echo \# Build script programs 
+for script in $scripts
+do
+    build_loc=${targetPath}/${script##*/}
+    ln -s $(pwd)/${script#*/} ${build_loc} -f -v
+done
---
+++ docs/me.md
@@ -0,0 +1,15 @@
+## Daily Routine
+
+Baviour should be done from high-level to low-level
+
+### Research Follow-up
+
+- Twitter
+- ArXiv
+  - Restoration
+  - Diffusion
+
+### Research 
+
+### Programming
+
