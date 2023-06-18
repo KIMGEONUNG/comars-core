81da5e66f331f0e386d31f4a2b8576866435beaa
comar <saywooong@gmail.com>
2023-06-18 10:45:02 +0900

    update: auto

--- vimconfig/nvim/lua/user/nvimdap.lua
+++ vimconfig/nvim/lua/user/nvimdap.lua
@@ -1,36 +1,50 @@
 local dap = require('dap')
 local dapui = require("dapui")
 
+-- ONLY FOR DEBUGGING
+-- dap.set_log_level('TRACE')
+
 dap.adapters.python = {
   type = 'executable';
   command = os.getenv('HOME') .. '/anaconda3/bin/python';
   args = { '-m', 'debugpy.adapter' };
 }
 
--- dap.configurations.python = {
---   {
---     name = "Launch file";
---     type = 'python';
---     request = 'launch';
---     program = "${file}";
---     pythonPath = function()
---       return os.getenv('HOME') .. '/anaconda3/bin/python'
---     end;
---   },
--- }
-
-dap.configurations.python = {
-  {
-    name = "Launch file";
+dap.configurations.python = { {
+    name = "Launch config";
     type = 'python';
-    request = 'attach';
+    request = 'launch';
     program = "${file}";
-    connect = {host= "localhost"};
     pythonPath = function()
       return os.getenv('HOME') .. '/anaconda3/bin/python'
     end;
   },
 }
+
+dap.adapters.remote_python = {
+  type = 'server';
+  host = 'localhost'; -- this is the server-side host
+  port = 5715; -- this is the server-side port
+}
+
+dap.configurations.python = {
+  {
+    name = "Attach config";
+    type = 'remote_python';
+    request = 'attach';
+    port = 7777; -- this is the client-side port
+    host = "127.0.0.1";
+    pathMappings = {
+      {
+        localRoot = "${workspaceFolder}";
+        remoteRoot = ".";
+      }
+    };
+  },
+}
+
+-- this code should be located after the definition of configurations.<filetype>
+require('dap.ext.vscode').load_launchjs('launch.json', { debugpy = { 'py' } })
 
 vim.keymap.set('n', '<F5>', function() require('dap').continue() end)
 vim.keymap.set('n', '<F10>', function() require('dap').step_over() end)
