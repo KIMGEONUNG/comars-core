e952a875138f4980d4701020c489de282a3ea1c3
comar <saywooong@gmail.com>
2024-06-04 23:36:15 +0900

    update: auto

--- vimconfig/nvim/lua/user/nvimdap.lua
+++ vimconfig/nvim/lua/user/nvimdap.lua
@@ -11,6 +11,19 @@ Dap = {}
 local dap = require('dap')
 local dapui = require("dapui")
 
+--       
+vim.api.nvim_set_hl(0, 'DapBreakpoint', { ctermbg = 0, fg = '#993939', bg = '#31353f' })
+vim.api.nvim_set_hl(0, 'DapLogPoint', { ctermbg = 0, fg = '#61afef', bg = '#31353f' })
+vim.api.nvim_set_hl(0, 'DapStopped', { ctermbg = 0, fg = '#98c379', bg = '#31353f' })
+
+vim.fn.sign_define('DapBreakpoint', { text='', texthl='DapBreakpoint', linehl='DapBreakpoint', numhl='DapBreakpoint' })
+vim.fn.sign_define('DapBreakpointCondition', { text='ﳁ', texthl='DapBreakpoint', linehl='DapBreakpoint', numhl='DapBreakpoint' })
+vim.fn.sign_define('DapBreakpointRejected', { text='', texthl='DapBreakpoint', linehl='DapBreakpoint', numhl= 'DapBreakpoint' })
+vim.fn.sign_define('DapLogPoint', { text='', texthl='DapLogPoint', linehl='DapLogPoint', numhl= 'DapLogPoint' })
+vim.fn.sign_define('DapStopped', { text='', texthl='DapStopped', linehl='DapStopped', numhl= 'DapStopped' })
+
+-- vim.fn.sign_define('DapBreakpoint', {text='•', texthl='red', linehl='', numhl=''})
+
 -- Note that omitting the "dapui.setup({})" invokes error.
 -- This setup method has a role of initializtion
 dapui.setup({})
